8782af44a3fd57d19199857e85852782808b6465
prateek <prateekjakhar123@gmail.com>
2024-12-05 14:35:04 +0530

    added versel json file

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API is running.....");
+});
+
 app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
